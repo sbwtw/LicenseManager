26004eef1cb9ce51feab982c7d994da2eea070ef
石博文 <sbw@sbw.so>
2016-02-04 14:39:35 +0800

    reconstruct

--- src/main.rs
+++ src/main.rs
@@ -7,48 +7,72 @@ use clap::App;
 
 use walkdir::WalkDir;
 
-use std::io;
-use std::io::SeekFrom;
 use std::io::prelude::*;
 use std::path::Path;
 use std::fs::OpenOptions;
 use std::fs::File;
 use std::thread;
+use std::sync::Arc;
 
-static LICENSE: &'static str = "/**
- * Copyright (C) 2015 Deepin Technology Co., Ltd.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- **/\n\n";
+struct Manager {
+    license: Arc<String>,
+}
 
-fn process_file(file: &Path) {
+impl Manager {
 
-    let file_name = file.to_string_lossy();
-
-    if !file_name.ends_with(".h") && !file_name.ends_with(".cpp") {
-        return;
+    fn new() -> Manager {
+        Manager {
+            license: Arc::new(String::new()),
+        }
     }
 
-    println!("process {:?}", file);
+    fn process_file(&self, file: &Path) {
 
+        let file_name = file.to_string_lossy();
 
-    // read old data
-    let mut fp = File::open(file).unwrap();
-    let mut buf = String::new();
-    fp.read_to_string(&mut buf);
+        if !file_name.ends_with(".h") && !file_name.ends_with(".cpp") {
+            return;
+        }
 
-    // truncate file
-    let mut fp = OpenOptions::new().write(true).truncate(true).open(file).unwrap();
+        println!("process {:?}", file);
 
-    thread::spawn(move || {
-        // write new data
-        fp.write_all(LICENSE.as_bytes()).unwrap();
-        fp.write_all(&buf.into_bytes()).unwrap();
-        fp.sync_data();
-    });
+        // read old data
+        let mut fp = File::open(file).unwrap();
+        let mut buf = String::new();
+        let _ = fp.read_to_string(&mut buf);
+
+        // truncate file
+        let mut fp = OpenOptions::new().write(true).truncate(true).open(file).unwrap();
+
+        let license = self.license.clone();
+        thread::spawn(move || {
+            // write new data
+            fp.write_all(license.as_bytes()).unwrap();
+            fp.write_all(&buf.into_bytes()).unwrap();
+            let _ = fp.sync_data();
+        });
+    }
+
+    fn process(&self, path: &str) {
+
+        for entry in WalkDir::new(path) {
+
+            if entry.is_err() {
+                continue;
+            }
+
+            let entry = entry.unwrap();
+            let file_type = entry.file_type();
+
+            if file_type.is_file() {
+                self.process_file(entry.path());
+            }
+        }
+    }
+
+    fn set_license(&mut self, license: &str) {
+        self.license = Arc::new(String::from(license));
+    }
 }
 
 fn main() {
@@ -61,6 +85,11 @@ fn main() {
                          .long("path")
                          .help("search path")
                          .takes_value(true))
+                    .arg(Arg::with_name("license")
+                         .short("l")
+                         .long("license")
+                         .help("specification a license file")
+                         .takes_value(true))
                     .get_matches();
 
     let search_path = match args.value_of("path") {
@@ -68,17 +97,15 @@ fn main() {
         _ => ".",
     };
 
-    for entry in WalkDir::new(search_path) {
+    let mut manager = Manager::new();
 
-        if entry.is_err() {
-            continue;
-        }
-
-        let entry = entry.unwrap();
-        let file_type = entry.file_type();
-
-        if file_type.is_file() {
-            process_file(entry.path());
-        }
-    }
+    manager.set_license("/**
+ * Copyright (C) 2015 Deepin Technology Co., Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ **/\n\n");
+    manager.process(search_path);
 }
